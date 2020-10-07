19034c01ac034fa352bb88619f214495ea02ab2d
kavyaraviprakash <70050485+kavyaraviprakash@users.noreply.github.com>
2020-10-07 14:28:11 -0500

    changing debug

--- mfscrm/settings.py
+++ mfscrm/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'hs7pq+9h67!n!&-o3myou!l2n!jq6f15=zq3&*s-750(dsjf(e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
